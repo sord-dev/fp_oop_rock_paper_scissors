83b53af76a128b189ef3b267f661cf83918dd045
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-02-25 09:17:39 +0000

    adding comments and making tweaks

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
+// this weapons object allows for extensiblity of the RPS game
 const weapons = {
-    rock: { weakTo: 'paper', strongTo: 'scissors' },
-    paper: { weakTo: 'scissors', strongTo: 'rock' },
-    scissors: { weakTo: 'rock', strongTo: 'paper' }
-}
+  rock: { weakTo: "paper", strongTo: "scissors" },
+  paper: { weakTo: "scissors", strongTo: "rock" },
+  scissors: { weakTo: "rock", strongTo: "paper" },
+};
 
 class RPS {
-    constructor() {
-        this.options = ["rock", "paper", "scissors"];
-        this.playerChoice = [];
-        this.computerChoice = [];
-        this.turn = 0;
+  constructor() {
+    // options is hardcoded atm but it 
+    // should probably be generated from the weapons as it validates each move
+    this.options = ["rock", "paper", "scissors"];
+    this.playerChoice = null;
+    this.computerChoice = null;
+  }
+
+  _roll() {
+    return this.options[Math.floor(Math.random() * this.options.length)];
+  }
+
+
+  // this function is somewhat repeated logic for both
+  _setPlayerChoice(value) {
+    if (this.options.indexOf(value) === -1) throw RangeError("no such choice");
+    this.playerChoice = value;
+  }
+
+  _setComputerChoice(value) {
+    if (this.options.indexOf(value) === -1) throw RangeError("no such choice");
+    this.computerChoice = value;
+  }
+
+  _compare(move1, move2) {
+    console.log({ player: move1, bot: move2 });
+    if (move1 === move2) console.log("draw!");
+
+    if (weapons[move1].strongTo === move2) {
+      console.log("player wins!");
+      return;
     }
 
-    _roll() {
-        return this.options[Math.floor(Math.random() * this.options.length)];
+    if (weapons[move1].weakTo === move2) {
+      console.log("bot wins!");
+      return;
     }
+  }
 
-    _setPlayerChoice(value) {
-        if (this.options.indexOf(value) === -1) throw RangeError("no such choice")
-        this.playerChoice.push(value)
-    }
+  start() {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-    _swapTurns() {
-        if (this.turn === 0) this.turn = 1;
-        else this.turn = 0
-    }
-
-    _setWinner(w) {
-        if (!w) this._winner = 'player';
-        else this._winner = 'computer';
-    }
-
-    _compare(move1, move2) {
-        if (move1 === move2) console.log('draw!');
-
-        if (weapons[move1].strongTo === move2) {
-            console.log('player wins!');
-            return;
-        }
-
-        if (weapons[move1].weakTo === move2) {
-            console.log('bot wins!');
-            return;
-        }
-    }
-
-    start() {
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        readline.question('Rock, Paper or Scissors? ', choice => {
-            if (!choice) throw new TypeError('incorrect input')
-            const c = String(choice).toLowerCase();
-            if (typeof choice !== 'string') throw new TypeError('incorrect input')
-            this._setPlayerChoice(c);
-            this.computerChoice.push(this._roll())
-            this._compare(this.computerChoice[this.computerChoice.length - 1], this.playerChoice[this.playerChoice.length - 1])
-            readline.close();
-        });
-    }
+    readline.question("Rock, Paper or Scissors? ", (choice) => {
+      if (!choice) throw new TypeError("incorrect input");
+      const c = String(choice).toLowerCase();
+      if (typeof choice !== "string") throw new TypeError("incorrect input");
+      this._setPlayerChoice(c);
+      this._setComputerChoice(this._roll());
+      this._compare(this.computerChoice, this.playerChoice);
+      readline.close();
+    });
+  }
 }
 
 const game = new RPS();
 
-game.start()
+game.start();
