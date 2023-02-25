0f1cf8386d700c3458d4b22d1174d20727a9f26e
Stef <stefansyrett17@gmail.com>
2023-02-25 09:01:03 +0000

    changing index.js content

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # fp_oop_rock_paper_scissors
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run start
+```
+
+## Improvements to be made
+
+- Run for more than one game
+- Track score
+- Cleaner Code?
+
--- index.js
+++ index.js
@@ -1,56 +1,68 @@
-class Dinosoaur {
-    constructor(name, age, diet) {
-        this.name = name;
-        this.age = age;
-        this.diet = diet;
+const weapons = {
+    rock: { weakTo: 'paper', strongTo: 'scissors' },
+    paper: { weakTo: 'scissors', strongTo: 'rock' },
+    scissors: { weakTo: 'rock', strongTo: 'paper' }
+}
+
+class RPS {
+    constructor() {
+        this.options = ["rock", "paper", "scissors"];
+        this.playerChoice = [];
+        this.computerChoice = [];
+        this.turn = 0;
     }
 
-    get roar() {
-        console.log(`${this.name} the ${this.species} roars.`)
+    _roll() {
+        return this.options[Math.floor(Math.random() * this.options.length)];
     }
 
-    feed(food) {
-        if (!food) return;
-        food !== this.diet
-            ? console.log(`${this.name} only eats ${this.diet}.`)
-            : console.log(`${this.name} the ${this.species} thanks you for the ${food}`)
+    _setPlayerChoice(value) {
+        if (this.options.indexOf(value) === -1) throw RangeError("no such choice")
+        this.playerChoice.push(value)
+    }
+
+    _swapTurns() {
+        if (this.turn === 0) this.turn = 1;
+        else this.turn = 0
+    }
+
+    _setWinner(w) {
+        if (!w) this._winner = 'player';
+        else this._winner = 'computer';
+    }
+
+    _compare(move1, move2) {
+        if (move1 === move2) console.log('draw!');
+
+        if (weapons[move1].strongTo === move2) {
+            console.log('player wins!');
+            return;
+        }
+
+        if (weapons[move1].weakTo === move2) {
+            console.log('bot wins!');
+            return;
+        }
+    }
+
+    start() {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Rock, Paper or Scissors? ', choice => {
+            if (!choice) throw new TypeError('incorrect input')
+            const c = String(choice).toLowerCase();
+            if (typeof choice !== 'string') throw new TypeError('incorrect input')
+            this._setPlayerChoice(c);
+            this.computerChoice.push(this._roll())
+            this._compare(this.computerChoice[this.computerChoice.length - 1], this.playerChoice[this.playerChoice.length - 1])
+            readline.close();
+        });
     }
 }
 
-class Trex extends Dinosoaur {
-    constructor(name, age) {
-        super(name, age, 'meat')
-        this.species = 'Trex';
-    }
+const game = new RPS();
 
-    charge(target) {
-        if (!target instanceof Dinosoaur) return;
-        console.log(`the enraged ${this.species} charges at the ${target.species}`)
-    }
-
-    static exstinct = false;
-
-    static toggleExtinction() {
-        Dinosoaur.exstinct = !Dinosoaur.exstinct;
-    }
-}
-
-class Stegosaurus extends Dinosoaur {
-    constructor(name, age) {
-        super(name, age, 'leaves')
-        this.species = 'Stegosaurus';
-    }
-
-    static exstinct = false;
-
-    static toggleExtinction() {
-        Dinosoaur.exstinct = !Dinosoaur.exstinct;
-    }
-}
-
-const Kyle = new Trex('Kyle', 3);
-const Jim = new Stegosaurus('Jim', 27);
-
-Trex.toggleExtinction()
-
-console.log(Trex.exstinct)
+game.start()
